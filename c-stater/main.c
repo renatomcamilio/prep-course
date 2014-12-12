3fc87b8b007f942554c292e59faeb34ca460c0e9
Renato Camilio <renatomcamilio@gmail.com>
2014-12-11 16:39:40 -0800

    Initial Commit

---
+++ c-stater/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-stater
+//
+//  Created by Renato Camilio on 12/11/14.
+//  Copyright (c) 2014 Renato Camilio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
